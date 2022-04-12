bc75f379a849fcef996345e420bc4a8133f22994
W3itch <team@w3itch.io>
2022-04-12 18:21:23 +0800

    fix: weely link in stde bar

--- docs/.vuepress/getSidebar.ts
+++ docs/.vuepress/getSidebar.ts
@@ -1,37 +1,44 @@
-import * as fs from 'fs';
-import * as path from 'path';
+import * as fs from "fs";
+import * as path from "path";
 
-const getSidebar = (basePath=path.join(__dirname, '..')) => {
+const getSidebar = (basePath = path.join(__dirname, "..")) => {
   const sidebar = [];
   const beforePath = process.cwd();
   process.chdir(basePath);
 
-  const parseDir = (dir: string, base='') => {
+  const parseDir = (dir: string, base = "") => {
     // ignore hidden files
-    const files: string[] = fs.readdirSync(path.join(basePath, base)).filter((sub) => !sub.startsWith('.'));
+    const files: string[] = fs
+      .readdirSync(path.join(basePath, base))
+      .filter((sub) => !sub.startsWith("."));
 
     files.forEach((file) => {
       let sidebarChildren = sidebar;
-      base.split('/').filter((e) => e).forEach((dir) => {
-        sidebarChildren = sidebarChildren.find(e => e.text === dir).children;
-      });
+      base
+        .split("/")
+        .filter((e) => e)
+        .forEach((dir) => {
+          sidebarChildren = sidebarChildren.find(
+            (e) => e.text === dir
+          ).children;
+        });
 
       if (fs.lstatSync(path.join(base, file)).isDirectory()) {
         sidebarChildren.push({
           text: file,
           collapsible: true,
           children: [],
-        })
+        });
         parseDir(file, path.join(base, file));
       } else {
         sidebarChildren.push({
           text: path.parse(file).name,
-          link: path.join('/', base, file),
-        })
+          link: path.join(".", base, file),
+        });
       }
-    })
-  }
-  parseDir('.');
+    });
+  };
+  parseDir(".");
 
   process.chdir(beforePath);
   return sidebar;
